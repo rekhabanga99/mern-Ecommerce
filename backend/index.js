81b4c76ad5ca23bde2bf23145c9a18969031f8d9
My Name <rekha.banga@habilelabs.io>
2023-04-25 15:38:05 +0530

    frontend

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const User = require("./db/Users");
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.post("/register", async (req, res) => {
   let user = new User(req.body);
@@ -15,4 +15,16 @@ app.post("/register", async (req, res) => {
   });
 });
 
+app.post("/login", async (req, res) => {
+  let user = await User.findOne(req.body).select("-password");
+  if (req.body.email && req.body.password) {
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ result: "No user found" });
+    }
+  }else {
+    res.send({ result: "No user found" });
+  }
+});
 app.listen(5000, () => console.log("server is runninng at 5000"));
